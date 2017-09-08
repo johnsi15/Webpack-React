ff5cc87f8346bb0fe1d1280ac989e6074ad9b7fe
John serrano <andrey_201015@hotmail.com>
2017-09-08 16:41:29 -0500

    Agregando request con superagent, consumiendo api con react

--- server.js
+++ server.js
@@ -5,6 +5,26 @@ app.use(express.static(__dirname + '/public'));
 
 const port = 3000;
 
+// Ejemplo de consumo de api con react
+const users = [
+  {
+    nombre: 'John',
+    edad: 24
+  },
+  {
+    nombre: 'Andrey',
+    edad: 25
+  }
+];
+
+app.get('/api/users', (req, res) => {
+  res.json({
+    users: users
+  });
+  // Tambien puedo enviar un array de objects
+  // res.json(users);
+});
+
 app.listen(port, () => {
   console.log(`Server on port ${port}`)
 });
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,45 @@
 import React, { Component } from 'react';
 
+// Documentation https://visionmedia.github.io/superagent/
+// GitHub -> https://github.com/visionmedia/superagent
+import request from 'superagent';
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      users: []
+    }
+  }
+  componentDidMount() {
+    // Nota: tener en cuenta el scope de las function, 
+    // en este caso no afecta por la @function => 
+    request
+      .get('/api/users')
+      .end((err, res) => {
+        if(err) return console.log(`Algo salio mal ${err}`); 
+        // console.log(res);
+        // Con res.body traemos un object of json y no un text.
+        // console.log(res.body);
+        const users = res.body.users;
+        this.setState({
+          users: users
+        });
+      });
+  }
+
   render() {
+    // console.log(this.state.users);
+    const users = this.state.users.map( (user, index) => {
+      return <li key={ index }>{ user.nombre } { user.edad }</li>
+    });
     return (
-      <h1>My aplication con react :D</h1>
+      <div>
+        <h1>My aplication con react :D</h1>
+        <ul>
+          { users }
+        </ul>
+      </div>
     )
   }
 }
